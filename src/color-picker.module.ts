6dfd7f693e4faa8b162052e7a7ee4fb57e0f8f63
Janne Julkunen <janne.julkunen@zef.fi>
2016-09-07 23:11:20 +0300

    Imported working module file

--- src/color-picker.module.ts
+++ src/color-picker.module.ts
@@ -2,12 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ColorPickerService} from './color-picker.service';
-import {ColorPickerDirective, DialogComponent, TextDirective, SliderDirective} from './color-picker.directive';
+import {ColorPickerDirective} from './color-picker.directive';
 
 @NgModule({
     imports: [CommonModule],
     providers: [ColorPickerService],
-    declarations: [ColorPickerDirective, DialogComponent, TextDirective, SliderDirective],
-    exports: [CommonModule, ColorPickerDirective, DialogComponent, TextDirective, SliderDirective]
+    declarations: [ColorPickerDirective],
+    exports: [CommonModule, ColorPickerDirective]
 })
 export class ColorPickerModule {}
